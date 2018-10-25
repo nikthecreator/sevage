685406ce788ea4c542952a6d5a2f6c37a2641a03
nikthecreator <nikifor.shapran@gmail.com>
2018-10-25 23:50:26 +0300

    commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "280px";
+    document.getElementById("main").style.marginRight = "50px";
+}
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginRight= "0";
+}

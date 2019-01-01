1ff28670959cf2763cb8276c09e9d215c43d209e
nikthecreator <nikifor.shapran@gmail.com>
2019-01-01 14:04:21 +0300

    make navbar

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 
 function openNav() {
-    document.getElementById("mySidebar").style.width = "280px";
-    document.getElementById("main").style.marginRight = "50px";
-}
-function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginRight= "0";
-}
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }

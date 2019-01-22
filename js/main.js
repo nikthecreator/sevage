89a9d1a911c5fad6f5d522904b653f0abcba23c4
nikthecreator <nikifor.shapran@gmail.com>
2019-01-22 19:32:11 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -21,3 +21,47 @@ for (i = 0; i < acc.length; i++) {
     } 
   });
 }
+
+
+
+$('.sl').slick({
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  dots: true,
+  infinite: true,
+  speed: 500,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+
+
+

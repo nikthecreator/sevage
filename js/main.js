e4f1f4a8b7be016180bf97489697079a20c148ff
nikthecreator <nikifor.shapran@gmail.com>
2019-02-07 11:17:21 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -31,7 +31,49 @@ $('.sl').slick({
   autoplaySpeed: 2000,
   dots: false,
   infinite: true,
+  prevArrow: null,
+  nextArrow: null,
   speed: 500,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+$('.sl-1').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  dots: false,
+  infinite: true,
+  speed: 500,
+  prevArrow: $('.prev'),
+  nextArrow: $('.next'),
   responsive: [
     {
       breakpoint: 1024,
